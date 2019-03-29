ade1bb8dfb8a3dae5dbaa40153d9fe3fc2cd292d
Kin <kaizerneos@gmail.com>
2019-03-29 15:37:15 +0800

    added Sign Up

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request,redirect,flash,url_for
+from werkzeug.security import generate_password_hash
+from models.user import User
+# import hashlib
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -9,12 +11,38 @@ users_blueprint = Blueprint('users',
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
+    
+#sign up display
+@users_blueprint.route('/sign_up', methods=['GET'])
+def sign_up():
+    return render_template('users/sign_up.html')
 
+#sign up add
+@users_blueprint.route('/sign_up/new', methods=['POST'])
+def sign_up_new():
+    username = request.form['username']
+    
+    password = request.form['pwd']
+    # password = hashlib.md5(password.encode())
+    # password = password.hexdigest()
+    
+    email = request.form['email']
 
-@users_blueprint.route('/', methods=['POST'])
-def create():
+    if len(username) != 0 and len(password) != 0 and (len(email) != 0):
+        hashed_pwd = generate_password_hash(password)
+
+        u = User(username=username,password=hashed_pwd,email=email)
+        if u.save():
+            flash(username + ' Creation Successful!')
+            return redirect('/new')
+        else:
+            return render_template('users/sign_up.html', name=username, errors=u.errors)   
+
+    
+
+@users_blueprint.route('/new/<id>', methods=['POST'])
+def create(id):
     pass
-
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
